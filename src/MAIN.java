a0548b72f275058d513b51873f9a259f80a439eb
Sheryar-Ahmad <sheryarahmad311@gmail.com>
2026-04-21 10:44:10 +0500

    Programs

--- src/MAIN.java
+++ src/MAIN.java
@@ -38,5 +38,13 @@ public class MAIN {
         float greater=students[0].maximum(students);
 
         System.out.println("Student with greater cgpa is: "+greater);
+
+        // Part B
+        int [][]matrix={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for (int i=0;i< matrix.length;i++){
+            sum+=matrix[i][i];
+        }
+        System.out.println(sum);
     }
 }
