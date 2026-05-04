8c977b0bad14836c1ca89a308802c7df83476c2f
Sheryar-Ahmad <sheryarahmad311@gmail.com>
2026-05-04 22:21:02 +0500

    made it working now it will store the elements at the start and also at the end and then display

--- src/linked_List.java
+++ src/linked_List.java
@@ -1,6 +1,6 @@
 class Practice{
     Node head;
-    class Node{
+    static class Node{
         int data;
         Node next;
         Node(int data){
@@ -11,36 +11,35 @@ class Practice{
 
     // inserting elements in the first
     public void insertAtStart(int data){
-        Node newNode=new Node(data);
+        Node newNode= new Node(data);
         if (head == null){
             head=newNode;
             return;
         }
-        else{
             newNode.next=head;
             head=newNode;
-        }
+
     }
 
     //inserting the elements in the list in the end
     public void insertAtEnd(int data){
-        Node newNode=new Node(data);
+        Node newNode= new Node(data);
         if (head == null){
             head=newNode;
             return;
         }
-        else{
            Node currNode=head;
-           while (currNode != null){
+           while (currNode.next != null){
                currNode=currNode.next;
            }
            currNode.next=newNode;
-        }
+
     }
+
     public void printList(){
         Node currNode=head;
         while (currNode != null){
-            System.out.println(currNode.data + "->");
+            System.out.print(currNode.data + "->");
             currNode=currNode.next;
         }
         System.out.println("Null");
@@ -50,5 +49,12 @@ class Practice{
 public class linked_List {
     public static void main(String[] args) {
 
+        Practice ll = new Practice();
+        ll.insertAtStart(2);
+        ll.printList();
+        ll.insertAtEnd(3);
+        ll.printList();
+        ll.insertAtStart(1);
+        ll.printList();
     }
 }
