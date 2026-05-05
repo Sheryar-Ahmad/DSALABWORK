c7a1bf731aa9bf156451a746a87875176bafcfcc
Sheryar-Ahmad <sheryarahmad311@gmail.com>
2026-05-05 11:32:33 +0500

    Update it

--- src/myStack.java
+++ src/myStack.java
@@ -1,67 +1,70 @@
 public class myStack {
-    private static int nsize=5;
-    int []arr=new int[nsize];
+    private int nsize= 5;
+    int []arr= new int[nsize];
     int top=-1;
 
     public boolean isEmpty(){
-        if (top <= -1) // if the top get incremented by one like there is one element that it would return the false which means
-            // that the stack is not empty
-            return true;
+        if (top <= -1)
+        return true;
         else
             return false;
     }
 
     public boolean isFull(){
-        if (top >= nsize-1) // it would show that if the stack is not full then return false
+        if (top >= nsize-1)
             return true;
         else
             return false;
     }
 
-    public void push(int value) { // here the value would be entered by the user
-        if (isFull()) {
-            System.out.println("Stack is full");
-            return;
-        } else {
-            top++; // here the value of top would be incremented and become 0 from -1
-            arr[top] = value;  //here the index 0 would store the value
-            System.out.println( value + ": is pushed into stack ");
+    public void push(int value){
+        if (isFull()){
+            System.out.println("Stack is Full, No more elements can be added");
+        }
+        else {
+            top++;
+            arr[top]=value;
+            System.out.println("Element Added to Stack: "+value);
         }
     }
-    public int pop(int value) {
-        if (isEmpty()) {
-            System.out.println("The stack is underflow");
+
+    public int pop(){
+        if (isEmpty()){
+            System.out.println("Stack is Empty");
             return 0;
-        } else {
-            System.out.println(arr[top] + "Removed form the stack");
+        }
+        else{
+            System.out.println("Element removed is : "+arr[top]);
             return top--;
         }
     }
 
     public void peek(){
         if (isEmpty()){
-            System.out.println("Stack is Empty");
+            System.out.println("Stack Underflow");
         }
         else{
-            System.out.println(arr[top]+ " :is at the top of the stack");
+            System.out.println("The peek value is: "+arr[top]);
         }
-        }
+    }
 
-    public int size() {
+    public int size(){
         return (top+1);
     }
 
+    public void printstack(){
+        for (int i=size()-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        myStack s1= new myStack();
-        s1.push(1);
-
-        // Size of the stack
-        System.out.println("Size of the Stack: " +s1.size());
-
-        s1.push(2);
-        s1.push(3);
-
-        s1.peek();
-
+        myStack stack=new myStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.printstack();
     }
 }
